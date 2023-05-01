17b2ccb062697e3d270f72ef8de7226aa15c78da
Nick Menzhuber <nickmenzhuber@gmail.com>
2023-04-30 19:21:11 -0500

    lesson one done, lesson two partially done

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is our employee-getting code now
+            List<string> employees = GetEmployees();
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine() ?? "";
+                if (input == "")
+                {
+                    break;
+                }
+
+                employees.Add(input);
+            }
+            // This is important!
+            return employees;
+        }
+    }
+}
